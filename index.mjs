977008f7388914814ac7e2d207252972c73e5cb1
Raymond Li <raymond.r.li@outlook.com>
2024-04-20 19:32:51 -0700

    Set the no-cache to "/" only.

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import http from 'node:http';
 import { createBareServer } from '@tomphttp/bare-server-node';
 import express from 'express';
 import { hostname } from "node:os";
-import {authorize, sendToUnauthorizedPage} from './auth-utils.js';
+import { authorize, sendToUnauthorizedPage } from './auth-utils.js';
 
 const httpServer = http.createServer();
 
@@ -14,43 +14,43 @@ const bareServer = createBareServer('/y/');
 
 httpServer.on('request', async (req, res) => {
   const isAuthorized = await authorize(req, res)
-  res.setHeader("Cache-control", "no-cache")
-
+  if (req.url == "/") {
+    res.setHeader("Cache-control", "no-cache")
+  }
   if (!isAuthorized) {
     res.writeHead(301, { Location: "https://quiz-genius.coducation.tech" });
     res.end();
     return
   }
 
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeRequest(req, res);
-	} else {
-		app(req, res);
-	}
-  });
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
 
 httpServer.on('upgrade', (req, socket, head) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeUpgrade(req, socket, head);
-	} else {
-		socket.end();
-	}
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
 httpServer.on('listening', () => {
-	const address = httpServer.address();
-    console.log("Listening on:");
-    console.log(`\thttp://localhost:${address.port}`);
-    console.log(`\thttp://${hostname()}:${address.port}`);
-    console.log(
-      `\thttp://${
-        address.family === "IPv6" ? `[${address.address}]` : address.address
-      }:${address.port}`
-    );
+  const address = httpServer.address();
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
 });
 
 
 
 httpServer.listen({
-	port: 3000,
+  port: 3000,
 });
